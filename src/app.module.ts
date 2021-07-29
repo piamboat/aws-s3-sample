3593c7631358bb64dd7e99e1bda56619671d0384
piamsak <piamsak@daytech.co.th>
2021-07-29 18:07:54 +0700

    init images

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from 'typeorm.config';
+import { ImagesModule } from './images/images.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    ImagesModule,
   ],
 })
 export class AppModule { }
